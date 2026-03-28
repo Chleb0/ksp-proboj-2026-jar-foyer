8f2b7e576210fe82ed5afa7232f576b357fa3d0a
Chleb0 <103438418+Chleb0@users.noreply.github.com>
2026-03-29 00:15:01 +0100

    todo+progress

--- players/python/player.py
+++ players/python/player.py
@@ -9,7 +9,10 @@ import torch
 
 class Player(PlayerInterface):
     memory: PPOMemory
-    train_mode: bool
+    train_mode: bool = True
+
+    lt_alive: int = 0
+    lt_tomstones: int = 0
 
     @staticmethod
     def log(*args):
@@ -23,11 +26,22 @@ class Player(PlayerInterface):
         torch.save(self.memory, "memory.pt")
     
     def load_memory(self) -> None:
-        memory: PPOMemory = torch.load(r"C:\Users\Sebik\Documents\pornboj\ksp-proboj-2026-jar-foyer\players\python\memory.pt")
+        try:
+            memory: PPOMemory = torch.load(r"C:\Users\Sebik\Documents\pornboj\ksp-proboj-2026-jar-foyer\players\python\memory.pt")
+            Player.log("Dojebal som načítavanie dát kokotko")
+        except:
+            memory: PPOMemory = {
+                "board": [],
+                "extra": [],
+                "actions": [],
+                "log_probs": [],
+                "rewards": [],
+                "dones": []
+            }
 
     def get_turn(self, world: World) -> List[Move]:
         if self.train_mode: return self.get_turn_train(world)
-        
+
         moves = []
         for id, ant in world.alive_shades.items():
             neighbours = ant.position.get_neighbouring()
@@ -39,7 +53,49 @@ class Player(PlayerInterface):
         return moves
     
     def get_turn_train(self, world: World) -> List[Move]:
+        moves = []
+        for id, ant in world.alive_shades.items():
+            
+            
+            # HLADIK TU DOPLN VECI KTORE RATAS
+            self.memory["board"].append(board) #toto chce byt Tensor z knihovne torch, 11x11x layery ktore chceme
+            self.memory["extra"].append(extra)  #toto chcu byt tie features
+            self.memory["actions"].append(action.item())
+            self.memory["log_probs"].append(log_prob.item())
+            self.memory["rewards"].append(reward)
+            self.memory["dones"].append(done)
+
+            self.update_lt(world)
+
+        return moves
+
+    def update_lt(self, world: World) -> None:
+        self.lt_alive = self.shades_cnt(world)
+
+        self.lt_tomstones = self.tom_cnt(world)
+
+    def shades_cnt(self, world: World) -> int:
+        out = 0
+        for id, ant in world.alive_shades.items():
+            if ant.owner == world.my_id:
+                out+=1
+        return out
+
+    def tom_cnt(self, world: World) -> int:
+        out = 0
+        for tom in world.alive_tombstones:
+            if tom.owner == world.my_id:
+                out += 1
+        return out
+
+    def eval_last_move(self, world: World):
+        shade_diff = self.shades_cnt(world) - self.lt_alive
+        tom_diff = self.tom_cnt(world) - self.lt_tomstones
+
+    
+    def eval_ghost_turn(self, shade: Shade, world: World) -> float:
         pass
+        
 
 if __name__ == "__main__":
     game = Game(Player())
